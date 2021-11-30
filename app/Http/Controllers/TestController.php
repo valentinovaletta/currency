eaa80eb731392532be5b475206211b391ed9a085
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2021-11-30 05:01:21 +0200

    first commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        return "TestController";
+    }
+}
