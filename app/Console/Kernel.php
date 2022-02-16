bf2e072d261bceda0ed1f6fe6997bb9b1a25ac22
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2022-02-16 15:02:28 +0200

    time for cron task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(new GetDataFromApi)->everyTenMinutes();
+        $schedule->call(new GetDataFromApi)->dailyAt('12:00');
     }
 
     /**
